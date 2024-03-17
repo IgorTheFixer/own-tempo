af93c317b6338b441a8df1defdefd2d39255a6b5
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2024-03-17 01:00:07 -0400

    created dashboard page and tweaked routing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,20 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
 
 const LandingPage = () =>{
+  const { isLoaded, isSignedIn, user } = useUser();
+  const router = useRouter()
+
+  if (isLoaded || isSignedIn) {
+    router.push("/dashboard")
+  }
+
   return(
+    //TODO: If user is signed in, render a loading state instead of the beloww
     <div>
       Landing Page (first page seen)
       <div>
